e8135665498ab037da3757776b3fb66496ffdef7
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-08-09 00:29:56 +0800

    [fix] missing import

--- utils/dead_lock_avoidance_agent.py
+++ utils/dead_lock_avoidance_agent.py
@@ -6,7 +6,7 @@ from flatland.core.env_observation_builder import DummyObservationBuilder
 from flatland.envs.agent_utils import RailAgentStatus
 from flatland.envs.rail_env import RailEnv, RailEnvActions, fast_count_nonzero
 
-from reinforcement_learning.policy import HeuristicPolicy, DummyMemory
+from our_policies.policy import HeuristicPolicy, DummyMemory
 from utils.agent_action_config import map_rail_env_action
 from utils.shortest_distance_walker import ShortestDistanceWalker
 
@@ -63,8 +63,11 @@ class DeadlockAvoidanceShortestDistanceWalker(ShortestDistanceWalker):
 
         if len(self.opp_agent_map.get(handle, [])) == 0:
             if self.switches.get(position, None) is None:
-                self.shortest_distance_agent_map[(handle, position[0], position[1])] = 1
-        self.full_shortest_distance_agent_map[(handle, position[0], position[1])] = 1
+                self.shortest_distance_agent_map[(
+                    handle, position[0], position[1])] = 1
+        self.full_shortest_distance_agent_map[(
+            handle, position[0], position[1])] = 1
+
 
 class DeadLockAvoidanceAgent(HeuristicPolicy):
     def __init__(self, env: RailEnv, action_size, enable_eps=False, show_debug_plot=False):
@@ -107,7 +110,8 @@ class DeadLockAvoidanceAgent(HeuristicPolicy):
             for w in range(self.env.width):
                 pos = (h, w)
                 for dir in range(4):
-                    possible_transitions = self.env.rail.get_transitions(*pos, dir)
+                    possible_transitions = self.env.rail.get_transitions(
+                        *pos, dir)
                     num_transitions = fast_count_nonzero(possible_transitions)
                     if num_transitions > 1:
                         if pos not in self.switches.keys():
@@ -128,7 +132,8 @@ class DeadLockAvoidanceAgent(HeuristicPolicy):
 
     def build_agent_position_map(self):
         # build map with agent positions (only active agents)
-        self.agent_positions = np.zeros((self.env.height, self.env.width), dtype=int) - 1
+        self.agent_positions = np.zeros(
+            (self.env.height, self.env.width), dtype=int) - 1
         for handle in range(self.env.get_num_agents()):
             agent = self.env.agents[handle]
             if agent.status == RailAgentStatus.ACTIVE:
@@ -152,19 +157,24 @@ class DeadLockAvoidanceAgent(HeuristicPolicy):
             if agent.status < RailAgentStatus.DONE:
                 next_step_ok = self.check_agent_can_move(handle,
                                                          shortest_distance_agent_map[handle],
-                                                         self.shortest_distance_walker.same_agent_map.get(handle, []),
-                                                         self.shortest_distance_walker.opp_agent_map.get(handle, []),
+                                                         self.shortest_distance_walker.same_agent_map.get(
+                                                             handle, []),
+                                                         self.shortest_distance_walker.opp_agent_map.get(
+                                                             handle, []),
                                                          full_shortest_distance_agent_map)
                 if next_step_ok:
-                    next_position, next_direction, action, _ = self.shortest_distance_walker.walk_one_step(handle)
-                    self.agent_can_move.update({handle: [next_position[0], next_position[1], next_direction, action]})
+                    next_position, next_direction, action, _ = self.shortest_distance_walker.walk_one_step(
+                        handle)
+                    self.agent_can_move.update(
+                        {handle: [next_position[0], next_position[1], next_direction, action]})
 
         if self.show_debug_plot:
             a = np.floor(np.sqrt(self.env.get_num_agents()))
             b = np.ceil(self.env.get_num_agents() / a)
             for handle in range(self.env.get_num_agents()):
                 plt.subplot(a, b, handle + 1)
-                plt.imshow(full_shortest_distance_agent_map[handle] + shortest_distance_agent_map[handle])
+                plt.imshow(
+                    full_shortest_distance_agent_map[handle] + shortest_distance_agent_map[handle])
             plt.show(block=False)
             plt.pause(0.01)
 
@@ -179,7 +189,8 @@ class DeadLockAvoidanceAgent(HeuristicPolicy):
         next_step_ok = True
         for opp_a in opp_agents:
             opp = full_shortest_distance_agent_map[opp_a]
-            delta = ((my_shortest_walking_path - opp - agent_positions_map) > 0).astype(int)
+            delta = ((my_shortest_walking_path - opp -
+                     agent_positions_map) > 0).astype(int)
             if np.sum(delta) < (3 + len(opp_agents)):
                 next_step_ok = False
             v = self.agent_can_move_value.get(handle, np.inf)
